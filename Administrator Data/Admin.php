1e24fe43a67c9a24aaa3a3ed3634fb05f062e4c6
AngelesAngelo <geloa1515@gmail.com>
2024-05-14 14:13:06 +0800

    Angelo commit

--- Administrator Data/Admin.php
+++ Administrator Data/Admin.php
@@ -50,4 +50,37 @@
       </ul>
     </nav>
 </body>
+
+<main>
+  <section>
+    <div class="leftbox">
+      <div class="leftcontent">
+          <h1>Announcement</h1>
+          <p>Legislative Library Service is responsible for the selection, acquisition and compilation of reference materials relevant to legislation. It organizes and maintains various library collections and service facilities to strengthen information and reference services in the Senate. It provides reference and research assistance to the Senators and their staff, other legislative researchers, Secretariat officials and employees of the Senate.</p>
+      </div>
+    </div>
+    <div class="updates">
+      <ul>
+        <li>
+          <div class="time">
+            <h2>13 May</h2>
+          </div>
+          <div class="upddetails">
+            <h3>Objectives</h3>
+            <p>To provide services and facilities responsive to and anticipatory of the reference and research requirements of the Members of the Senate, the legislative researchers, as well as the Secretariat officers and employees. This calls for:
+
+1. the selection, evaluation, acquisition and organization of various information resources, in printed and electronic forms;
+
+2. the provision of necessary tools for retrieval and delivery of information (e.g. indexes, catalogs, on-line public access catalog (OPAC), accession and shelf-list records);
+
+3. the rendering of reference and research assistance service;
+
+4. the dissemination of bibliographies, annotated reading lists and copies of journal articles relevant to legislative concerns or of interest to Members of the Senate and Secretariat officers;</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
+
 </html>
